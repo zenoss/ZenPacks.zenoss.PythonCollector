f2068470ba40a035bed3b0633717146320e584f0
Chet Luther <chet.luther@gmail.com>
2014-09-25 15:27:29 -0500

    Post Release: Version 1.5.2 -> 1.5.3dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.PythonCollector"
-VERSION = "1.5.2"
+VERSION = "1.5.3dev"
 AUTHOR = "Zenoss"
 LICENSE = "All Rights Reserved"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
