40578028863048c224ad2c5ae7a3b91e14731ec8
Chet Luther <chet.luther@gmail.com>
2015-01-27 17:04:01 -0600

    Start 1.7.0 release: Version 1.6.2dev -> 1.7.0dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.PythonCollector"
-VERSION = "1.6.2dev"
+VERSION = "1.7.0dev"
 AUTHOR = "Zenoss"
 LICENSE = "All Rights Reserved"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
